6a3aae350606e3f3ea356cd1f92dd2840e8727d8
mridhulak98 <98010367+mridhulak98@users.noreply.github.com>
2022-04-20 10:40:03 +0530

    Refactored wla (#981)
    * Refactored wla
    
    * Refactoring wlagent

--- pkg/wlagent/clients/workload_service_client.go
+++ pkg/wlagent/clients/workload_service_client.go
@@ -5,69 +5,13 @@
 package clients
 
 import (
-	"net/url"
-	"strings"
-
 	"github.com/intel-secl/intel-secl/v5/pkg/clients/wlsclient"
 	wlsModel "github.com/intel-secl/intel-secl/v5/pkg/model/wls"
 	"github.com/intel-secl/intel-secl/v5/pkg/wlagent/constants"
 	"github.com/pkg/errors"
 	"github.com/spf13/viper"
+	"net/url"
 )
-
-// GetImageFlavorKey method is used to get the image flavor-key from the workload service
-func GetImageFlavorKey(imageUUID, hardwareUUID string) (wlsModel.FlavorKey, error) {
-	log.Trace("clients/workload_service_client:GetImageFlavorKey() Entering")
-	defer log.Trace("clients/workload_service_client:GetImageFlavorKey() Leaving")
-	var flavorKeyInfo wlsModel.FlavorKey
-	wlsClientFactory, err := wlsclient.NewWLSClientFactory(viper.GetString(constants.WlsApiUrlViperKey), viper.GetString(constants.AasBaseUrlViperKey),
-		viper.GetString(constants.WlaUsernameViperKey), viper.GetString(constants.WlaPasswordViperKey), constants.TrustedCaCertsDir)
-	if err != nil {
-		return flavorKeyInfo, errors.Wrap(err, "Error while instantiating WLSClientFactory")
-
-	}
-
-	flavorsClient, err := wlsClientFactory.FlavorsClient()
-	if err != nil {
-		return flavorKeyInfo, errors.Wrap(err, "Error while instantiating FlavorsClient")
-	}
-
-	flavorKeyInfo, err = flavorsClient.GetImageFlavorKey(imageUUID, hardwareUUID)
-	if err != nil {
-		// Return error as nil in case of http response code 404, to support docker images with no image flavor association
-		if strings.Contains(err.Error(), "404") {
-			return flavorKeyInfo, nil
-		}
-		return flavorKeyInfo, errors.Wrap(err, "Error while retrieving Flavor-Key")
-	}
-
-	log.Debug("client/workload_service_client:GetImageFlavorKey() Successfully retrieved Flavor-Key")
-	return flavorKeyInfo, nil
-}
-
-// GetImageFlavor method is used to get the image flavor from the workload service
-func GetImageFlavor(imageID, flavorPart string) (wlsModel.SignedImageFlavor, error) {
-	log.Trace("clients/workload_service_client:GetImageFlavor() Entering")
-	defer log.Trace("clients/workload_service_client:GetImageFlavor() Leaving")
-	var flavor wlsModel.SignedImageFlavor
-	wlsClientFactory, err := wlsclient.NewWLSClientFactory(viper.GetString(constants.WlsApiUrlViperKey), viper.GetString(constants.AasBaseUrlViperKey),
-		viper.GetString(constants.WlaUsernameViperKey), viper.GetString(constants.WlaPasswordViperKey), constants.TrustedCaCertsDir)
-	if err != nil {
-		return flavor, errors.Wrap(err, "Error while instantiating WLSClientFactory")
-	}
-
-	flavorsClient, err := wlsClientFactory.FlavorsClient()
-	if err != nil {
-		return flavor, errors.Wrap(err, "Error while instantiating FlavorsClient")
-	}
-
-	flavor, err = flavorsClient.GetImageFlavor(imageID, flavorPart)
-	if err != nil {
-		return flavor, errors.Wrap(err, "Error while getting ImageFlavor")
-	}
-
-	return flavor, nil
-}
 
 // GetKeyWithURL method is used to get the image flavor-key from the workload service
 func GetKeyWithURL(keyUrl string, hardwareUUID string) (wlsModel.ReturnKey, error) {
--- pkg/wlagent/flavor/key_retrieval.go
+++ pkg/wlagent/flavor/key_retrieval.go
@@ -6,68 +6,13 @@
 package flavor
 
 import (
-	"github.com/google/uuid"
+	cLog "github.com/intel-secl/intel-secl/v5/pkg/lib/common/log"
 	wlsModel "github.com/intel-secl/intel-secl/v5/pkg/model/wls"
 	wlsclient "github.com/intel-secl/intel-secl/v5/pkg/wlagent/clients"
 	"github.com/intel-secl/intel-secl/v5/pkg/wlagent/util"
 )
 
-// RetrieveKey retrieves an Image decryption key
-// It uses the hardwareUUID that is fetched from the Platform Info library
-func RetrieveKey(keyID string) ([]byte, bool) {
-	log.Trace("flavor/key_retrieval:RetrieveKey Entering")
-	defer log.Trace("flavor/key_retrieval:RetrieveKey Leaving")
-	//check if the key is cached by filtercriteria imageUUID
-	var err error
-	var flavorKeyInfo wlsModel.FlavorKey
-	var tpmWrappedKey []byte
-
-	if imageKeyID[keyID] == "" {
-		log.Errorf("flavor/key_retrieval:RetrieveKey() unable to get the image ID for given key ID %s", keyID)
-		return nil, false
-	}
-	imageUUID := imageKeyID[keyID]
-
-	// get the platform-info
-	hInfo := util.GetPlatformInfo()
-	if hInfo == nil {
-		log.Errorf("flavor/key_retrieval:RetrieveKey() unable to retrieve Platform Info")
-		return nil, false
-	}
-
-	// get host hardware UUID
-	log.Debug("Retrieving host hardware UUID...")
-	hardwareUUID := hInfo.HardwareUUID
-
-	log.Debugf("The host hardware UUID is :%s", hardwareUUID)
-
-	//get flavor-key from workload service
-	log.Infof("Retrieving image-flavor-key for image %s from WLS", imageUUID)
-	flavorKeyInfo, err = wlsclient.GetImageFlavorKey(imageUUID, hardwareUUID)
-	if err != nil {
-		log.Errorf("flavor/key_retrieval:RetrieveKey() error retrieving the image flavor and key: %s", err.Error())
-		log.Tracef("%+v", err)
-		return nil, false
-	}
-
-	if flavorKeyInfo.Flavor.Meta.ID == uuid.Nil {
-		log.Infof("Flavor does not exist for the image %s", imageUUID)
-		return nil, true
-	}
-
-	if flavorKeyInfo.Flavor.EncryptionRequired {
-		// if the WLS response includes a key, cache the key on host
-		if len(flavorKeyInfo.Key) > 0 {
-			// get the key from WLS response
-			tpmWrappedKey = flavorKeyInfo.Key
-			return tpmWrappedKey, true
-		}
-
-		return nil, false
-	}
-
-	return nil, false
-}
+var log = cLog.GetDefaultLogger()
 
 // RetrieveKeyWithURL retrieves an Image decryption key
 // It uses the hardwareUUID that is fetched from the Platform Info library
