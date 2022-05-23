336c1db9c5986aa343068d9af6a643ba282d1cb7
imaheshwarans <66402782+mshamn6x@users.noreply.github.com>
2022-05-23 00:04:32 -0400

    revoked wpm changes (#1004)

--- pkg/wpm/util/encrypt.go
+++ pkg/wpm/util/encrypt.go
@@ -5,85 +5,17 @@
 package util
 
 import (
-	"bytes"
-	"crypto/aes"
-	"crypto/cipher"
 	"crypto/rand"
 	"crypto/rsa"
 	"crypto/sha512"
 	"crypto/x509"
-	"encoding/binary"
 	"encoding/pem"
-	"github.com/intel-secl/intel-secl/v5/pkg/lib/common/crypt"
 	cLog "github.com/intel-secl/intel-secl/v5/pkg/lib/common/log"
-	cMsg "github.com/intel-secl/intel-secl/v5/pkg/lib/common/log/message"
 	"github.com/pkg/errors"
-	"io"
 	"io/ioutil"
-	"unsafe"
 )
 
 var log = cLog.GetDefaultLogger()
-
-func Encrypt(imagePath string, privateKeyLocation string, encryptedFileLocation string, wrappedKey []byte) error {
-	log.Trace("pkg/wpm/util/encrypt.go:Encrypt() Entering")
-	defer log.Trace("pkg/wpm/util/encrypt.go:Encrypt() Leaving")
-
-	var encryptionHeader crypt.EncryptionHeader
-
-	// reading image file
-	image, err := ioutil.ReadFile(imagePath)
-	if err != nil {
-		return errors.Wrap(err, "Error reading the image file")
-	}
-
-	key, err := UnwrapKey(wrappedKey, privateKeyLocation)
-	if err != nil {
-		return errors.Wrap(err, "Error while unwrapping the key")
-	}
-	// creating a new cipher block of 128 bits
-	block, err := aes.NewCipher(key)
-	if err != nil {
-		return errors.Wrap(err, "Error initializing cipher")
-	}
-
-	gcm, err := cipher.NewGCM(block)
-	if err != nil {
-		return errors.Wrap(err, "Error creating a cipher block")
-	}
-
-	log.Infof("pkg/util/encrypt.go:Encrypt() %s", cMsg.EncKeyUsed)
-
-	// assigning a 12 byte empty array to store random value
-	iv := make([]byte, 12)
-	// reading random value into the byte array
-	if _, err = io.ReadFull(rand.Reader, iv); err != nil {
-		return errors.Wrap(err, "Error creating random IV value")
-	}
-
-	copy(encryptionHeader.MagicText[:], crypt.EncryptionHeaderMagicText)
-	copy(encryptionHeader.EncryptionAlgorithm[:], crypt.GCMEncryptionAlgorithm)
-	copy(encryptionHeader.IV[:], iv)
-	copy(encryptionHeader.Version[:], crypt.EncryptionHeaderVersion)
-	encryptionHeader.OffsetInLittleEndian = uint32(unsafe.Sizeof(encryptionHeader))
-
-	encryptionHeaderSlice := &bytes.Buffer{}
-	err = binary.Write(encryptionHeaderSlice, binary.LittleEndian, encryptionHeader)
-	if err != nil {
-		return errors.Wrap(err, "Error while writing encryption header struct values to buffer")
-	}
-
-	// The first 44 bytes of the encrypted file is the encryption header and
-	// the rest is the data.
-	encryptedDataWithHeader := gcm.Seal(encryptionHeaderSlice.Bytes(), iv, image, nil)
-	err = ioutil.WriteFile(encryptedFileLocation, encryptedDataWithHeader, 0600)
-	if err != nil {
-		return errors.Wrap(err, "Error during writing the encrypted image to file")
-	}
-
-	log.Info("pkg/wpm/util/encrypt.go:Encrypt() Successfully encrypted image")
-	return nil
-}
 
 func UnwrapKey(wrappedKey []byte, privateKeyLocation string) ([]byte, error) {
 	log.Trace("pkg/wpm/util/encrypt.go:UnwrapKey() Entering")
