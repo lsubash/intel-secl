6edf3b442635f6a49cd16b957da15a4d9a58fc41
srinics <srinivasan.rajendran@intel.com>
2022-06-07 09:34:11 +0530

    ISECL-21441: In CMS swagger document port number to be updated (#1012)

--- docs/shared/cms/version.go
+++ docs/shared/cms/version.go
@@ -19,7 +19,7 @@ package cms
 //     description: Successfully retrieved the version.
 //     content: text/plain
 //
-// x-sample-call-endpoint: https://cms.com:8443/cms/v1/version
+// x-sample-call-endpoint: https://cms.com:8445/cms/v1/version
 // x-sample-call-output: |
 //   Service Name: Certificate Management Service
 //   Version: v3.4.0-0f0162ea
