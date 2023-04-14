987deaf3c72636ad576f66942d1cc9ab5d814ebd
Subash Lakkimsetti <subash.lakkimsetti@intel.com>
2023-04-14 15:02:18 -0700

    tagent cmd constant update for TEP configuration
    Signed-off-by: Subash Lakkimsetti <subash.lakkimsetti@intel.com>

--- cmd/tagent/constants.go
+++ cmd/tagent/constants.go
@@ -7,9 +7,9 @@ package main
 const (
 	ServiceUserName = "trustagent"
 	ServiceDir      = "trustagent/"
-	LogDir          = "/var/log/" + ServiceDir
+	LogDir          = "/home/root/tep_luks_dev/log/" + ServiceDir
 	LogFile         = LogDir + ServiceUserName + ".log"
 	HttpLogFile     = LogDir + ServiceUserName + "-http.log"
 	SecurityLogFile = LogDir + ServiceUserName + "-security.log"
-	TagentUserName  = "tagent"
+	TagentUserName  = "root"
 )
